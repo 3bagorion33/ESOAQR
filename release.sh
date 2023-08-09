1197cc7e76e0158fd165b0f1d3f45aaba72c8711
3bagorion33 <orion_33@bk.ru>
2023-08-09 12:27:53 +0600

    Addedd autorelease action

--- release.sh
+++ release.sh
@@ -1,14 +1,11 @@
-for f in $(find . -name "*.lua"); do
-    if [ $(cat $f | grep logger | grep -v "^ *--" | wc -l) -gt 0 ]; then
-        echo -e "Attention! There are loggers in the source code\n"
-        read
-    fi
-done
+#!/bin/bash
+
 VERSION="$(cat ESOAQR.txt  | grep "## Version:" | cut -d":" -f2 | xargs)"
-rm ESOAQR*.zip
+sed -i "s/RELEASE_TAG:.*/RELEASE_TAG: v$VERSION/" release_config.yml
+rm -f ESOAQR*.zip
 mkdir ESOAQR
 mkdir ESOAQR/luaqrcode
 cp luaqrcode/qrencode.lua ESOAQR/luaqrcode/qrencode.lua
 cp ESOAQR.txt ESOAQR.lua Bindings.xml ESOAQR
-7z a -r ESOAQR-$VERSION.zip ESOAQR
+zip -r ESOAQR.zip ESOAQR/*
 rm -rf ESOAQR
